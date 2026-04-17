fa2fe0df1eb9b05aabae9fe5a87071dca2d18606
aaronguero <aaronguero@live.com>
2026-04-17 02:11:04 +0000

    Auto-commit agent changes

--- whambible-static/verses.js
+++ whambible-static/verses.js
@@ -48,3 +48,6 @@ const VERSES = [
 const BOOKS_OT = ["Genesis","Exodus","Leviticus","Numbers","Deuteronomy","Joshua","Judges","Ruth","1 Samuel","2 Samuel","1 Kings","2 Kings","1 Chronicles","2 Chronicles","Ezra","Nehemiah","Esther","Job","Psalms","Proverbs","Ecclesiastes","Isaiah","Jeremiah","Lamentations","Ezekiel","Daniel","Hosea","Joel","Amos","Micah","Nahum","Habakkuk","Zephaniah","Haggai","Zechariah","Malachi"];
 const BOOKS_NT = ["Matthew","Mark","Luke","John","Acts","Romans","1 Corinthians","2 Corinthians","Galatians","Ephesians","Philippians","Colossians","1 Thessalonians","2 Thessalonians","1 Timothy","2 Timothy","Titus","Philemon","Hebrews","James","1 Peter","2 Peter","1 John","2 John","3 John","Jude","Revelation"];
 const ALL_BOOKS = [...BOOKS_OT, ...BOOKS_NT];
+
+// Expose globally for Firebase challenge engine
+window.WHAM_VERSES = VERSES;
